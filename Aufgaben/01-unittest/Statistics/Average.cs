5fdd0de73eba5b27039661be51f7c2a7622f00dc
BeStRavenclaw <Ben.Stadelmann2@gmail.com>
2022-10-30 15:19:51 +0100

    Task 4.3: Added ArgumentException For Emty List in Mean

--- Aufgaben/01-unittest/Statistics/Average.cs
+++ Aufgaben/01-unittest/Statistics/Average.cs
@@ -10,6 +10,10 @@ namespace Statistics
             int sum = 0;
             double mean = 0.0;
 
+            if (numbers.Count == 0)
+                throw new ArgumentException("There are no numbers for the Mean!");
+
+
             foreach (int number in numbers)
             {
                 sum += number;
