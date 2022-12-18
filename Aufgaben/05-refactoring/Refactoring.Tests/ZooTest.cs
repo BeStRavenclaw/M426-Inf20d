b5635281a9170ea31a070a68b6109097818079e2
BeStRavenclaw <Ben.Stadelmann2@gmail.com>
2022-12-18 16:24:09 +0100

    Tests for Zoo

--- Aufgaben/05-refactoring/Refactoring.Tests/ZooTest.cs
+++ Aufgaben/05-refactoring/Refactoring.Tests/ZooTest.cs
@@ -1,3 +1,4 @@
+using Refactoring.Animals;
 using Xunit;
 
 namespace Refactoring.Tests;
@@ -5,8 +6,35 @@ namespace Refactoring.Tests;
 public class ZooTest
 {
     [Fact]
-    public void TestMakeNoise()
+    public void TestDogMakeNoise()
     {
-        // TODO
+        Animal dog = new Dog("Simp");
+        string noise = "Simp says 'woof'";
+
+        string actual = Zoo.MakeNoise(dog);
+
+        Assert.Equal(noise, actual);
+    }
+
+    [Fact]
+    public void TestHorseMakeNoise()
+    {
+        Animal horse = new Horse("Simon");
+        string noise = "Simon says 'neigh'";
+
+        string actual = Zoo.MakeNoise(horse);
+
+        Assert.Equal(noise, actual);
+    }
+
+    [Fact]
+    public void TestCowMakeNoise()
+    {
+        Animal cow = new Cow("Markus");
+        string noise = "Markus says 'mooh'";
+
+        string actual = Zoo.MakeNoise(cow);
+
+        Assert.Equal(noise, actual);
     }
 }
